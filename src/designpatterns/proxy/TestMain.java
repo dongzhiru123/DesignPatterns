3973efcc20d6417f9e08be84513e6ade06db8946
dzr <2201357779@qq.com>
2020-07-19 12:30:40 +0800

    代理设计模式

--- src/designpatterns/proxy/TestMain.java
+++ src/designpatterns/proxy/TestMain.java
@@ -1,6 +1,9 @@
-package designpatterns.proxy.staticproxy;
+package designpatterns.proxy;
 
-public class Test {
+import designpatterns.proxy.staticproxy.FactoryTest;
+import designpatterns.proxy.staticproxy.ProxyTest;
+
+public class TestMain {
     public static void main(String[] args) {
         FactoryTest factoryTest = new FactoryTest();
         ProxyTest proxyTest = new ProxyTest(factoryTest);
